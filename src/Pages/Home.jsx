bd36d4fb0361167e8a1c40b9be4fc1301fb7f002
Alee-72 <hassanrazaalvi217@gmail.com>
2021-01-31 12:18:22 +0500

    updated
    UOL APP

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,43 @@
+
+import React from "react";
+import { Box, Button, Flex, Heading } from "@chakra-ui/core";
+import logo from "../assets/images/logo-complete.png";
+import { useAuth0 } from "@auth0/auth0-react";
+
+const Home = () => {
+  const { loginWithRedirect } = useAuth0();
+  return (
+    <Box>
+      <Box style={{ height: "100vh" }} background={`url(landing-image.png)`}>
+        <Flex m="auto" p="3rem" w="90%" align="center" justify="space-between">
+          
+          
+        </Flex>
+
+        <Flex
+          m="auto"
+          h="70%"
+          direction="column"
+          justify="center"
+          align="center"
+        >
+          <Heading color="white" textAlign="center">
+            UNIVERSITY OF THAL
+          </Heading>
+         
+          <Button
+            my="2rem"
+            variantColor="orange.800"
+            bg="brand.orange.800"
+            w="100px"
+            onClick={loginWithRedirect}
+          >
+            Sign in
+          </Button>
+        </Flex>
+      </Box>
+    </Box>
+  );
+};
+
+export default Home;
